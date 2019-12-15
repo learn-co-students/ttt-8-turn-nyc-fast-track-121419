86befcfa0c35cb353061186ad7c7c9766d0d742c
Jean Baptiste Elias Asong <jeanasong46@yahoo.com>
2019-12-15 16:14:09 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,5 @@
+#display_board
+Should accept a board as an argument and print out the current state of the board for the user.
+#valid_move?
+Should accept a board and an index from the user and return true if the index is within the correct range of 0-8 and is currently unoccupied by an X or O token
+#position_taken?
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/turn.rb"
+r'equire_relative' "../lib/turn.rb"
 
 describe './lib/turn.rb' do
 
